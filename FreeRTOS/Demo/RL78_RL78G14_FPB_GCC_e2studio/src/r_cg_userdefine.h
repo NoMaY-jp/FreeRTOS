b1d20b2b601c93fa0094a34c8c61b799cca89c4f
NoMaY (a user of Japan.RenesasRulz.com) <NoMaY-jp@outlook.com>
2020-12-06 19:13:09 +0900

    Add Renesas RL78/G13 GNURL78 and CC-RL RTOS Demo
    The source files are almost the same source files in the following zip files of the following thread other than the version number and URLs in comment at the header part of the source files.
    
    https://japan.renesasrulz.com/cafe_rene/f/forum21/4826/freertos-fit-cg-pdg2-csplus/27325#27325
    FreeRTOS_Demo_RL78_multiple_RenesasCCRL_CS+_20180218.zip
    FreeRTOS_Demo_RL78_multiple_RenesasCCRL_e2studio_20180218.zip
    
    https://japan.renesasrulz.com/cafe_rene/f/forum21/4826/freertos-fit-cg-pdg2-csplus/27179#27179
    FreeRTOS_Demo_RL78_multiple_GCC_e2studio_20180204.zip

---
+++ FreeRTOS/Demo/RL78_RL78G14_FPB_GCC_e2studio/src/r_cg_userdefine.h
@@ -0,0 +1,58 @@
+/***********************************************************************************************************************
+* DISCLAIMER
+* This software is supplied by Renesas Electronics Corporation and is only intended for use with Renesas products.
+* No other uses are authorized. This software is owned by Renesas Electronics Corporation and is protected under all
+* applicable laws, including copyright laws. 
+* THIS SOFTWARE IS PROVIDED "AS IS" AND RENESAS MAKES NO WARRANTIESREGARDING THIS SOFTWARE, WHETHER EXPRESS, IMPLIED
+* OR STATUTORY, INCLUDING BUT NOT LIMITED TO WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+* NON-INFRINGEMENT.  ALL SUCH WARRANTIES ARE EXPRESSLY DISCLAIMED.TO THE MAXIMUM EXTENT PERMITTED NOT PROHIBITED BY
+* LAW, NEITHER RENESAS ELECTRONICS CORPORATION NOR ANY OF ITS AFFILIATED COMPANIES SHALL BE LIABLE FOR ANY DIRECT,
+* INDIRECT, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES FOR ANY REASON RELATED TO THIS SOFTWARE, EVEN IF RENESAS OR
+* ITS AFFILIATES HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+* Renesas reserves the right, without notice, to make changes to this software and to discontinue the availability 
+* of this software. By using this software, you agree to the additional terms and conditions found by accessing the 
+* following link:
+* http://www.renesas.com/disclaimer
+*
+* Copyright (C) 2011, 2017 Renesas Electronics Corporation. All rights reserved.
+***********************************************************************************************************************/
+
+/***********************************************************************************************************************
+* File Name    : r_cg_userdefine.h
+* Version      : CodeGenerator for RL78/G13 V2.05.00.06 [10 Nov 2017]
+* Device(s)    : R5F100LG
+* Tool-Chain   : GCCRL78
+* Description  : This file includes user definition.
+* Creation Date: 2018/02/04
+***********************************************************************************************************************/
+
+#ifndef _USER_DEF_H
+#define _USER_DEF_H
+
+/***********************************************************************************************************************
+User definitions
+***********************************************************************************************************************/
+
+/* Start user code for function. Do not edit comment generated here */
+
+#include "FreeRTOS.h"
+
+#if INTIT_vect == 0x38
+/*
+ * INT_IT (0x38)
+ */
+void vPortTickISR(void) __attribute__ ((interrupt)); /* Note this vector table definition is used with lots of RL78 chips, some of which have the INTIT vector here. */
+#endif
+#if INTIT_vect == 0x3C
+/*
+ * INT_IT (0x3C)
+ */
+void vPortTickISR(void) __attribute__ ((interrupt)); /* Note this vector table definition is used with lots of RL78 chips, some of which have the INTIT vector here. */
+#endif
+/*
+ * INT_BRK_I (0x7E)
+ */
+void vPortYield(void) __attribute__ ((interrupt));
+
+/* End user code. Do not edit comment generated here */
+#endif
